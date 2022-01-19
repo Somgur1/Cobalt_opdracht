e334d8300bbde8922ed0ab0a4e6285679a9b6066
Somgur1 <Vincedewit@live.nl>
2022-01-19 20:47:31 +0100

    Add project files.

---
+++ Cobalt_opdracht/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cobalt_opdracht
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Kobalt = new List<string>();
+            Kobalt.Add("Arne");
+            Kobalt.Add("Brenda");
+            Kobalt.Add("Carly");
+            Kobalt.Add("Desley");
+
+           
+
+            foreach (var naam in Kobalt)
+            {
+
+            }
+
+        }
+
+        private static void Kobalt_opdracht1()
+        {
+            int cijfer = 1;
+
+            while (cijfer <= 100)
+            {
+                Console.WriteLine(cijfer);
+
+                if (cijfer % 3 == 0)
+                {
+                    Console.WriteLine("co");
+                }
+
+                if (cijfer % 5 == 0)
+                {
+                    Console.WriteLine("balt");
+                }
+
+                if (cijfer % 5 == 0 && cijfer % 3 == 0)
+                {
+                    Console.WriteLine("cobalt");
+                }
+                cijfer = cijfer + 1;
+            }
+        }
+    }
+}
