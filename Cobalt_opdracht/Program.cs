b7a5b56b2ceb3f5babca2b2bf2d5c53bb40794ae
Somgur1 <Vincedewit@live.nl>
2022-01-19 21:34:17 +0100

    Telt nu hoeveel letters in namen

--- Cobalt_opdracht/Program.cs
+++ Cobalt_opdracht/Program.cs
@@ -5,23 +5,60 @@ namespace Cobalt_opdracht
 {
     class Program
     {
-        static void Main(string[] args)
+        public class KobaltNamen
         {
-            var Kobalt = new List<string>();
-            Kobalt.Add("Arne");
-            Kobalt.Add("Brenda");
-            Kobalt.Add("Carly");
-            Kobalt.Add("Desley");
-
-           
-
-            foreach (var naam in Kobalt)
-            {
-
-            }
-
+            public string Name { get; set; }
+            public int Cijfer { get; set; }
         }
 
+
+
+        static void Main(string[] args)
+        {
+
+            var Kobalt = new List<KobaltNamen>//in word op afphabetische folgorde gezet
+        {
+           new KobaltNamen() { Name="Arne ", Cijfer = 1},
+            new KobaltNamen() { Name="Brenda ", Cijfer = 2},
+            new KobaltNamen() { Name="Carly ", Cijfer = 3},
+            new KobaltNamen() { Name="Desley ", Cijfer = 4},
+            new KobaltNamen() { Name="Esther ", Cijfer = 5},
+            new KobaltNamen() { Name="Huub ", Cijfer = 6},
+            new KobaltNamen() { Name="Kylian ", Cijfer = 7},
+            new KobaltNamen() { Name="Mick ", Cijfer = 8},
+            new KobaltNamen() { Name="Kylian ", Cijfer = 9},
+            new KobaltNamen() { Name="Mick ", Cijfer = 10},
+        };
+
+            foreach (KobaltNamen kobaltmensen in Kobalt)
+            {
+
+
+
+                int Count = 0;
+                foreach (char c in kobaltmensen.Name)
+                {
+                    if (c != ' ')
+                    {
+                        Count++;
+                    }
+                }
+                int k = Count;
+
+                Console.WriteLine(kobaltmensen.Name + k);
+
+            }
+        }
+    }
+}
+
+                
+                
+            
+
+        
+
+    /*
         private static void Kobalt_opdracht1()
         {
             int cijfer = 1;
@@ -45,7 +82,5 @@ namespace Cobalt_opdracht
                     Console.WriteLine("cobalt");
                 }
                 cijfer = cijfer + 1;
-            }
-        }
-    }
-}
+    */
+     
